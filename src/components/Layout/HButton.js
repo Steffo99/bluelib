6c5023e2c81d30168fc47ee280b5a97bda8c91d2
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-23 22:33:49 +0200

    0.12.3

--- src/components/Layout/HButton.js
+++ src/components/Layout/HButton.js
@@ -1,12 +1,22 @@
 import style from './HButton.less';
+import {concatClass} from "../../index";
 
 export default function (props) {
+	let validityClass = null;
+	if(props.validity === true) {
+		validityClass = style.valid;
+	}
+	else if(props.validity === false) {
+		validityClass = style.invalid;
+	}
+
+
 	return (
 		<label className={style.label}>
 			<div className={style.text}>{props.label}</div>
 			<button
 				onClick={props.disabled ? null : props.onClick}
-				className={style.button}
+				className={concatClass(style.button, validityClass)}
 				disabled={props.disabled}
 			>
 				{props.children}
--- src/components/Layout/HInput.js
+++ src/components/Layout/HInput.js
@@ -1,10 +1,19 @@
 import style from './HInput.less';
+import {concatClass} from "../../index";
 
 export default function (props) {
+	let validityClass = null;
+	if(props.validity === true) {
+		validityClass = style.valid;
+	}
+	else if(props.validity === false) {
+		validityClass = style.invalid;
+	}
+
 	return (
 		<label className={style.label}>
 			<div className={style.text}>{props.label}</div>
-			<input className={style.input} type={props.type} value={props.value}
+			<input className={concatClass(style.input, validityClass)} type={props.type} value={props.value}
 				   onChange={props.onChange} disabled={props.disabled} name={props.name}/>
 		</label>
 	);
