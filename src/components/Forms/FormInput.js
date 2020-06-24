d7ea69a44fb29ea56146e447ef1a3975bf686830
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-25 00:27:26 +0200

    0.13.4

--- src/components/Forms/FormInput.js
+++ src/components/Forms/FormInput.js
@@ -3,15 +3,14 @@ import style from "./FormRow.less"
 
 export default function (props) {
     return (
-        <FormRow
-            disabled={props.disabled}
-            validity={props.validity}
-            label={props.label}>
+        <FormRow {...props}>
             <input
                 class={style.contentschild}
                 type={props.type}
                 value={props.value}
-                onChange={props.onChange}
+                onChange={props.disabled ? null : props.onChange}
+                onFocus={props.disabled ? null : props.onFocus}
+                onBlur={props.disabled ? null : props.onBlur}
                 disabled={props.disabled}
                 name={props.name}/>
         </FormRow>
