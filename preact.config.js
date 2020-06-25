bf58982eeb9748ab978960714116e91c1422960d
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-25 18:26:06 +0200

    0.13.6

--- preact.config.js
+++ preact.config.js
@@ -2,13 +2,32 @@ const DefinePlugin = require("webpack/lib/DefinePlugin");
 
 
 export default function (config, env, helpers) {
-    config.module.rules[4].include = [env.source(".")]
-    config.module.rules[5].exclude = [env.source(".")]
+    config.module.rules[4].include = [env.source(".")];
+    config.module.rules[5].exclude = [env.source(".")];
 
     // noinspection JSUnresolvedVariable
     config.resolve.alias["react"] = "preact/compat";
     // noinspection JSUnresolvedVariable
     config.resolve.alias["react-dom"] = "preact/compat";
+
+    config.module.rules.push(
+        {
+            test: /\.nojekyll$/,
+            loader: 'file-loader',
+            options: {
+                name: '.nojekyll'
+            }
+        }
+    );
+    config.module.rules.push(
+        {
+            test: /CNAME$/,
+            loader: 'file-loader',
+            options: {
+                name: 'CNAME'
+            }
+        }
+    );
 
     config.plugins.push(
         new DefinePlugin({"process.env.RELEASE": `"${process.env.npm_package_version}"`})
