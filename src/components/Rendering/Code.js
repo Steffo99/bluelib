d38c63da7e62b0bdab69f44bcc26abc9794b12ba
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-10-14 02:19:36 +0200

    Add a default language context

--- src/components/Rendering/Code.js
+++ src/components/Rendering/Code.js
@@ -1,10 +1,14 @@
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import {monokai} from "react-syntax-highlighter/dist/cjs/styles/hljs";
 import stripTabs from "../../utils/stripTabs";
+import {useContext} from "preact/hooks";
+import CodeDefaultLanguage from "../../contexts/CodeDefaultLanguage";
 
 export default function(props) {
+    const language = useContext(CodeDefaultLanguage)
+
     return (
-        <SyntaxHighlighter language={props.language ? props.language : "plaintext"} style={monokai}>
+        <SyntaxHighlighter language={props.language ? props.language : language} style={monokai}>
             {stripTabs(String(props.children))}
         </SyntaxHighlighter>
     )
