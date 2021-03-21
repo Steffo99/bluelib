daec7d4805d47603d8b77f80f0b9316669b8dfec
Stefano Pigozzi <me@steffo.eu>
2021-03-21 02:08:41 +0100

    ⬆ Update to the latest bluelib version

--- src/components/Box/Readme.md
+++ src/components/Box/Readme.md
@@ -7,5 +7,11 @@ import Bluelib from "../Bluelib";
     <Box>
         Ciao!
     </Box>
+    <Box color={"#ff7d7d"}>
+        This is a red box.
+    </Box>
+    <Box color={"rgb(125, 125, 255)"}>
+        This is a blue box.
+    </Box>
 </Bluelib>
 ```
--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import useBluelibClassNames from "../../hooks/useBluelibClassNames";
 import PropTypes from "prop-types";
+import Color from "color";
+import {isString} from "../../utils"
 
 
-export default function Box({children, className}) {
+export default function Box({children, className, color}) {
+    let style = {}
+    if(color !== undefined) {
+        if(isString(color)) {
+            color = new Color(color)
+        }
+        style["--bluelib-color-r"] = color.red()
+        style["--bluelib-color-g"] = color.green()
+        style["--bluelib-color-b"] = color.blue()
+    }
+
     return (
-        <section className={useBluelibClassNames("panel panel-box", className)}>
+        <section className={useBluelibClassNames("panel panel-box", className)} style={style}>
             {children}
         </section>
     )
