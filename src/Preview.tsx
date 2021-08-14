9952291e17b3d7f52334517b7af00eb77812b294
Stefano Pigozzi <me@steffo.eu>
2021-08-14 17:02:47 +0200

    ✨ Initialize project

---
+++ src/Preview.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+export default function Preview() {
+    return <>
+        <div>
+            This should work.
+        </div>
+    </>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Preview from './Preview';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Preview />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('react-root')
 );
 
 // If you want to start measuring performance in your app, pass a function
