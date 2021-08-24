bffadb3c792c11b355aedc6bf7048457f560d3ee
Stefano Pigozzi <me@steffo.eu>
2021-08-24 16:05:37 +0200

    🔧 Change slightly the preview

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -2,11 +2,13 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import {Bluelib} from "./components/Bluelib";
 import * as Colors from "./utils/Colors"
+import {Chapter} from "./components/chapters/Chapter";
+import {Heading} from "./components/common/Heading";
+import {Panel} from "./components/panels/Panel";
 
-export default function Preview() {
-    return <>
-        <Bluelib theme={"hacker"} color={Colors.BuiltinColor.LIME} style={{minHeight: "100vh"}}>
-            This should work.
-        </Bluelib>
-    </>
+
+export function Preview({...props}): JSX.Element {
+    return (
+        <span>Nothing to render here.</span>
+    )
 }
