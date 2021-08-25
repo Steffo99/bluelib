dc7ff2610aa3c8ee5d18780d2bdd1abb73e44433
Stefano Pigozzi <me@steffo.eu>
2021-08-25 20:44:41 +0200

    🔧 Move index to the source root directory

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+export {Ruby} from "./components/annotations/Ruby"
+
+export {Chapter} from "./components/chapters/Chapter"
+
+export {Anchor} from "./components/common/Anchor"
+export {Heading} from "./components/common/Heading"
+
+export {Form} from "./components/forms/Form"
+
+export {Image} from "./components/images/Image"
+
+export {Area} from "./components/inputs/Area"
+export {Button} from "./components/inputs/Button"
+export {Checkbox} from "./components/inputs/Checkbox"
+export {Field} from "./components/inputs/Field"
+export {Multiselect} from "./components/inputs/Multiselect"
+export {Radio} from "./components/inputs/Radio"
+export {Select} from "./components/inputs/Select"
+
+export {LayoutFill} from "./components/layouts/LayoutFill"
+export {LayoutThreeCol} from "./components/layouts/LayoutThreeCol"
+export {Footer} from "./components/layouts/Footer"
+
+export {ListDescription} from "./components/lists/ListDescription"
+export {ListOrdered} from "./components/lists/ListOrdered"
+export {ListUnordered} from "./components/lists/ListUnordered"
+
+export {Box} from "./components/panels/Box"
+export {Dialog} from "./components/panels/Dialog"
+export {Panel} from "./components/panels/Panel"
+export {Parenthesis} from "./components/panels/Parenthesis"
+
+export {Abbreviation} from "./components/semantics/Abbreviation"
+export {BringAttention} from "./components/semantics/BringAttention"
+export {Cite} from "./components/semantics/Cite"
+export {Code} from "./components/semantics/Code"
+export {Definition} from "./components/semantics/Definition"
+export {Emphasis} from "./components/semantics/Emphasis"
+export {Idiomatic} from "./components/semantics/Idiomatic"
+export {Mark} from "./components/semantics/Mark"
+export {Quote} from "./components/semantics/Quote"
+export {Sample} from "./components/semantics/Sample"
+export {Strikethrough} from "./components/semantics/Strikethrough"
+export {Strong} from "./components/semantics/Strong"
+export {UAnnotation} from "./components/semantics/UAnnotation"
+export {Variable} from "./components/semantics/Variable"
+
+export {Separator} from "./components/separators/Separator"
+
+export {Table} from "./components/tables/Table"
+
+export {Bluelib} from "./components/Bluelib"
+export {Bluelib as default} from "./components/Bluelib"
