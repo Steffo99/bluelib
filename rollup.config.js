a1321098360d28488e9910f2c9edfd77690b539a
Stefano Pigozzi <me@steffo.eu>
2021-08-28 06:02:18 +0200

    🔨 Configure rollup

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import postcss from 'rollup-plugin-postcss'
+import typescript from "@rollup/plugin-typescript"
+import pkg from './package.json'
+
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named'
+    },
+    external: [
+        "@babel/runtime",
+        "react",
+        "react-dom",
+        "classnames",
+        "color",
+        "uuid",
+    ],
+    plugins: [
+        postcss({
+            modules: true,
+        }),
+        typescript(),
+        babel({
+            babelHelpers: "runtime",
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-transform-runtime"],
+            extensions: [".tsx", ".ts", ".jsx", ".js", ".mjs"],
+            exclude: 'node_modules/**',
+        }),
+        resolve(),
+        commonjs(),
+    ]
+}
