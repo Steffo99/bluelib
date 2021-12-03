5d96baccc6dccbfaef5dab2706ac5f87a8376d7c
Stefano Pigozzi <me@steffo.eu>
2021-12-03 17:31:06 +0100

    🐛 Properly display examples (fix #8)
    See storybookjs/storybook#12596

--- src/utils/Decorators.jsx
+++ src/utils/Decorators.jsx
@@ -4,12 +4,12 @@ import { Form as FormComponent } from "../components/forms/Form"
 import { Ruby as RubyComponent } from "../components/annotations/Ruby"
 
 
-export const Bluelib = Story => <BluelibComponent theme={"paper"} style={{backgroundColor: "transparent"}}><Story/></BluelibComponent>
+export const Bluelib = Story => <BluelibComponent theme={"paper"} style={{backgroundColor: "transparent"}}>{Story()}</BluelibComponent>
 
-export const Fill = Story => <div style={{height: "100vh"}}><Story/></div>
+export const Fill = Story => <div style={{height: "100vh"}}>{Story()}</div>
 
-export const Box = Story => <BoxComponent><Story/></BoxComponent>
+export const Box = Story => <BoxComponent>{Story()}</BoxComponent>
 
-export const Form = Story => <FormComponent><Story/></FormComponent>
+export const Form = Story => <FormComponent>{Story()}</FormComponent>
 
-export const Ruby = Story => <RubyComponent><Story/></RubyComponent>
+export const Ruby = Story => <RubyComponent>{Story()}</RubyComponent>
