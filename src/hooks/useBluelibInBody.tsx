c7666a619407ffb29a93fd3948a49470fbdf08a2
Stefano Pigozzi <me@steffo.eu>
2022-02-09 13:53:42 +0100

    Add typing for useBluelibInBody

--- src/hooks/useBluelibInBody.tsx
+++ src/hooks/useBluelibInBody.tsx
@@ -1,7 +1,8 @@
 import { useRef, useEffect } from "react"
+import { BluelibTheme } from "../types"
 import { useBluelib } from "./useBluelib"
 
-export function useBluelibInBody(theme) {
+export function useBluelibInBody(theme: BluelibTheme) {
     const ref = useRef<HTMLBodyElement | null>(null)
     useEffect(
         () => {
