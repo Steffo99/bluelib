d603f0ab1fce32fb128f613ee9844c212ab2a40b
Stefano Pigozzi <me@steffo.eu>
2023-05-25 01:20:38 +0200

    v9 (#64)
    * Work on improving examples
    * Fix toggles
    * Create the `unpkgTargets` and `yarnTargets` variables
    * Allow interaction again
    * Add media screen to color rules
    * Configure default colors and fonts
    * Configure panel min-width in the example page
    * Remove the need for a examples index.css
    * Place selectorsets in the same chapter as rulesets
    * Rework images and build script
    * Tweak todo rendering
    * Do not bother styling range
    * Create layouts and fix bold code
    * Update text and readme

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
-# Bluelib
+<center>
 
-Customizable, flexible and modular CSS library
+# ![](media/icon.png)<br>Bluelib
 
-\[ [**Demo + Documentation**](https://gh.steffo.eu/bluelib/examples/index.html) | [**npm**](https://www.npmjs.com/package/@steffo/bluelib) \]
+Customizable, flexible and modular CSS library.
 
-![Screenshot](media/screenshot-1.png)
+</center>
 
-_Background image is [**Nebula Galaxy Photo** by Fabrice Nerfin on Unsplash](https://unsplash.com/photos/puHQJZd3MDg)._
+## Links
+
+[![Demo](https://img.shields.io/website?label=demo&url=https%3A%2F%2Fbluelib.steffo.eu%2Fexamples%2Findex.html)](https://img.shields.io/website?label=demo&url=https%3A%2F%2Fbluelib.steffo.eu%2Fexamples%2Findex.html)
+ 
+[![On NPM](https://img.shields.io/npm/v/@steffo/bluelib)](https://www.npmjs.com/package/@steffo/bluelib)
+ 
+[![Chat](https://img.shields.io/matrix/bluelib:ryg.one?server_fqdn=matrix.ryg.one)](https://matrix.to/#/#bluelib:ryg.one)
+
+## Screenshots
+
+![Screenshot of the Royal Blue color theme](media/screenshot-1.png)
+![Screenshot of the Pixel Dawn color theme](media/screenshot-2.png)
+
+## Credits
+
+### Royal Blue
+
+- Background image: [**Nebula galaxy photo** by Fabrice Nerfin on Unsplash](https://unsplash.com/photos/puHQJZd3MDg)
+
+### Gestione Amber
+
+- Background image: [**Brown coffee beans on brown wooden table photo** by Anastasiia Chepinska on Unsplash](https://unsplash.com/photos/lcfH0p6emhw)
+
+### Pixel Dawn
+
+- Background image: **Pixel Dawn** by [Viktya](https://viktya.github.io) (original)
+
+### Purple Star
+
+- Background image: [**Calm body of water near alp mountains during nighttime photo** by kazuend on Unsplash](https://unsplash.com/photos/2KXEb_8G5vo)
--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,66 @@
 /**
+ * (Ab)use UNPKG to retrieve the latest Bluelib semantic version.
+ *
+ * @returns {Promise<string>}
+ */
+async function getLatestSemver() {
+	const resp = await fetch("https://unpkg.com/@steffo/bluelib/package.json")
+	const data = await resp.json()
+	return data.version
+}
+
+/**
+ * The latest version of Bluelib, or `undefined` if it has not been retrieved yet.
+ *
+ * @type {string | undefined}
+ */
+let version = undefined
+
+/**
+ * Initialize {@link version} by calling {@link getLatestSemver}.
+ */
+async function initVersion() {
+	console.debug("[Bluelib] Getting version info via UNPKG...")
+	version = await getLatestSemver()
+	console.info("[Bluelib] Latest Bluelib version is:", version)
+}
+
+
+/**
+ * {@link RegExp} to acquire the base name (`"classic"`) from a Less stylesheet id (`"less:dist-classic:root"`).
+ *
+ * @type {RegExp}
+ */
+const baseNameFromStyleIdRegex = new RegExp("^less:dist-([a-z-]+):root$")
+
+/**
+ * Acquire the base name (`"classic"`) from a Less stylesheet id (`"less:dist-classic:root"`).
+ *
+ * @param styleId The Less stylesheet id (`"less:dist-classic:root"`).
+ * @returns {string | undefined} The base name (`"classic"`).
+ */
+function getBaseNameFromStyleId(styleId) {
+	return styleId.match(baseNameFromStyleIdRegex)[1]
+}
+
+/**
+ * Generate the Less stylesheet id (`"less:dist-classic:root"`) from a base name (`"classic"`).
+ */
+function getStyleIdFromBaseName(baseName) {
+	return `less:dist-${baseName}:root`
+}
+
+/**
  * Check if the less compiler is done rendering Less stylesheets into CSS.
  *
  * @returns {boolean} `true` if the rendering is complete, `false` otherwise.
  */
 function isLessDone() {
-	const lessSheets = document.querySelectorAll("link[rel=\"stylesheet/less\"]")
-	const cssSheets = document.querySelectorAll("style")
+	const lessSheets = document.querySelectorAll("head link[rel=\"stylesheet/less\"]")
+	const cssSheets = document.querySelectorAll("head style")
 
 	return lessSheets.length === cssSheets.length
 }
-
 
 /**
  * Pause an async function until {@link isLessDone Less is done}.
@@ -17,69 +68,25 @@ function isLessDone() {
  * @returns {Promise<void>} Awaitable that waits until {@link isLessDone Less is done}.
  */
 async function sleepUntilLessIsDone() {
+	console.debug("[Bluelib] Waiting for Less to finish compiling stylesheets...")
 	while(!isLessDone()) {
 		await new Promise(resolve => setTimeout(resolve, 100))
-		console.debug("[Bluelib] Less seems to be done processing.")
 	}
+	console.debug("[Bluelib] Less seems to have finished compiling stylesheets.")
 }
 
 
 /**
- * List of stylesheet ids that should be enabled on page load.
+ * Return a map of base names to their corresponding {@link HTMLStyleElement}s.
  *
- * @type {string[]}
+ * @returns {{[string]: HTMLStyleElement}}
  */
-const enabledByDefault = [
-	"less:dist-base:root",
-	"less:dist-classic:root",
-	"less:dist-glass:root",
-	"less:dist-layouts-center:root",
-	"less:dist-colors-royalblue:root",
-	"less:dist-fonts-fira-ghpages:root",
-]
-
-
-/**
- * List of all `<style>` {@link HTMLElement}s with an id starting with `less:dist`.
- *
- * @type {{string: HTMLElement} | undefined}
- */
-let lessStyles = undefined
-
-/**
- * List of all `<style>` {@link HTMLElement}s with an id starting with `less:dist-colors`.
- *
- * @type {{string: HTMLElement} | undefined}
- */
-let lessColors = undefined
-
-/**
- * List of all `<style>` {@link HTMLElement}s with an id starting with `less:dist-fonts`.
- *
- * @type {{string: HTMLElement} | undefined}
- */
-let lessFonts = undefined
-
-/**
- * The background element.
- *
- * @type {HTMLElement | undefined}
- */
-let background = undefined
-
-
-/**
- * Find the `<style>` {@link HTMLElement}s with an id starting with `less:dist`, and compile them into a {`id`: `element`} object.
- *
- * @returns {{string: HTMLElement}}
- */
-function findLessStyles() {
-	// noinspection JSValidateTypes
+function getCompiledLessStylesheets() {
 	return [...document.styleSheets].filter(
 		(s) => s.ownerNode.id.startsWith("less:dist"),
 	).map(
 		(s) => (
-			{ [s.ownerNode.id]: s }
+			{ [getBaseNameFromStyleId(s.ownerNode.id)]: s }
 		),
 	).reduce(
 		(p, c) => (
@@ -90,128 +97,252 @@ function findLessStyles() {
 }
 
 /**
- * Find the `<style>` {@link HTMLElement}s with an id starting with `less:dist-colors`, and compile them into a {`id`: `element`} object.
+ * Object mapping base names to their corresponding {@link HTMLStyleElement}s. Needs to be initialized.
  *
- * @returns {{string: HTMLElement}}
+ * @type {{[string]: HTMLStyleElement} | undefined}
  */
-function findLessColors() {
-	// noinspection JSValidateTypes
-	return [...document.styleSheets].filter(
-		(s) => s.ownerNode.id.startsWith("less:dist-colors"),
-	).map(
-		(s) => (
-			{ [s.ownerNode.id]: s }
-		),
-	).reduce(
-		(p, c) => (
-			{ ...p, ...c }
-		),
-		{},
-	)
+let styleFromBaseName = undefined
+
+/**
+ * Initialize {@link styleFromBaseName} by calling {@link getCompiledLessStylesheets}.
+ */
+function initStyleFromBaseName() {
+	console.debug("[Bluelib] Detecting compiled styles...")
+	styleFromBaseName = getCompiledLessStylesheets()
+	console.info("[Bluelib] The following styles are available:", styleFromBaseName)
 }
 
 /**
- * Find the `<style>` {@link HTMLElement}s with an id starting with `less:dist-fonts`, and compile them into a {`id`: `element`} object.
- *
- * @returns {{string: HTMLElement}}
+ * The currently enabled selectorset.
+ * 
+ * @type {String}
  */
-function findLessFonts() {
-	// noinspection JSValidateTypes
-	return [...document.styleSheets].filter(
-		(s) => s.ownerNode.id.startsWith("less:dist-fonts"),
-	).map(
-		(s) => (
-			{ [s.ownerNode.id]: s }
-		),
-	).reduce(
-		(p, c) => (
-			{ ...p, ...c }
-		),
-		{},
-	)
+let selectorsetEnabled = "root";
+
+/**
+ * List of valid rulesets, in the order they should be enabled in.
+ */
+const rulesets = [
+	"base",
+	"classic",
+	"glass",
+	"fun",
+	"layouts-center",
+	"layouts-flex",
+	"colors-royalblue",
+	"colors-amber",
+	"colors-pixeldawn",
+	"colors-purplestar",
+	"fonts-fira-ghpages",
+]
+
+/**
+ * Function to pass to {@link sort} to sort a list of base names in the order of {@link rulesets}.
+ */
+function rulesetsSorter(a, b) {
+	return rulesets.indexOf(a) - rulesets.indexOf(b)
+}
+
+
+/**
+ * List of currently enabled rulesets, not in any particular order.
+ */
+const rulesetsEnabled = [
+	"base",
+	"classic",
+	"glass",
+	"layouts-center",
+	"colors-royalblue",
+	"fonts-fira-ghpages",
+]
+
+/**
+ * Sort {@link rulesetsEnabled} using {@link rulesetsSorter}.
+ */
+function sortRulesetsEnabled() {
+	console.debug("[Bluelib] Sorting rulesets:", rulesetsEnabled)
+	rulesetsEnabled.sort(rulesetsSorter)
+	console.debug("[Bluelib] Sorted rulesets:", rulesetsEnabled)
 }
 
 /**
- * Find and return the {@link HTMLElement} with the `layout-center-background` class.
- *
- * @returns {HTMLElement}
+ * Add a ruleset to {@link rulesetsEnabled}, if it doesn't already exist.
  */
-function findBackground() {
-	return document.getElementsByClassName("layout-center-background")[0]
+function enableRuleset(baseName) {
+	const pos = rulesetsEnabled.indexOf(baseName)
+	if(pos) {
+		rulesetsEnabled.push(baseName)
+	}
 }
 
 /**
- * Allows the user to dynamically interact with bluelib via the interactive components available on the page.
- *
- * Set the values of {@link lessStyles}, {@link lessColors}, {@link lessFonts} and {@link background}.
- *
- * Sets the enabled state of the all {@link lessStyles} depending on their inclusion in the {@link enabledByDefault} array.
- *
- * Enables and unfades all input elements with the class `ruleset-toggle`.
- *
- * @returns {Promise<void>}
+ * Remove a ruleset from {@link rulesetsEnabled}, if it exists.
  */
-async function enableChanges() {
+function disableRuleset(baseName) {
+	const pos = rulesetsEnabled.indexOf(baseName)
+	if(pos) {
+		rulesetsEnabled.splice(pos, 1)
+	}
+}
+
+/**
+ * {@link enableRuleset} or {@link disableRuleset}, based on the value of `shouldBeEnabled`.
+ */
+function setRuleset(baseName, shouldBeEnabled) {
+	if(shouldBeEnabled) {
+		enableRuleset(baseName)
+	}
+	else {
+		disableRuleset(baseName)
+	}
+}
+
+
+/**
+ * The `#js-unpkg-targets` element.
+ * 
+ * @type {HTMLDivElement | undefined}
+ */
+let unpkgTargets = undefined
+
+/**
+ * The `#js-yarn-targets` element.
+ * 
+ * @type {HTMLDivElement | undefined}
+ */
+let yarnTargets = undefined
+
+/**
+ * Initialize the {@link unpkgTargets} and {@link yarnTargets} variables.
+ */
+function initTargets() {
+	unpkgTargets = document.getElementById("js-unpkg-targets")
+	yarnTargets = document.getElementById("js-yarn-targets")
+}
+
+
+/**
+ * Synchronize {@link rulesetsEnabled} with the {@link document}.
+ */
+function sync() {
+	console.debug("[Bluelib] Syncing enabled rulesets:", rulesetsEnabled)
+	// Toggle Less-compiled stylesheets
+	for(const [baseName, element] of Object.entries(styleFromBaseName)) {
+		element.disabled = !rulesetsEnabled.includes(baseName)
+		console.debug("[Bluelib] State of", baseName, element, "changed to", !element.disabled)
+	}
+	unpkgTargets.innerHTML = rulesetsEnabled.map(baseName => `<code>&lt;link <wbr>rel=&quot;stylesheet&quot; <wbr>href=&quot;https://unpkg.com/@steffo/bluelib<wbr>@<b>${version}</b><wbr>/dist/<wbr><b>${baseName}</b>.<b>${selectorsetEnabled}</b>.min.css&quot;<wbr> type=&quot;text/css&quot; /&gt;</code>`).join("<br>")
+	yarnTargets.innerHTML = rulesetsEnabled.map(baseName => `<code>import &quot;@steffo/bluelib/dist/<wbr><b>${baseName}</b><wbr>.<b>${selectorsetEnabled}</b><wbr>.css&quot;;</code>`).join("<br>")
+	console.debug("[Bluelib] Done syncing enabled rulesets.")
+}
+
+/**
+ * Allow the user to interact with the various interactable components of the page.
+ */
+function allowInteraction() {
+	console.debug("[Bluelib] Enabling interaction on elements...")
+	for(const element of document.querySelectorAll("#panel-rulesets-toggleable input")) {
+		console.debug("[Bluelib] Enabling interaction on:", element)
+		element.disabled = false
+		element.addEventListener("input", onToggleableRulesetToggle)
+		element.parentElement.classList.remove("fade")
+	}
+	for(const element of document.querySelectorAll("#panel-rulesets-colors input")) {
+		console.debug("[Bluelib] Enabling interaction on:", element)
+		element.disabled = false
+		element.addEventListener("input", onColorRulesetSelection)
+		element.parentElement.classList.remove("fade")
+	}
+	for(const element of document.querySelectorAll("#panel-rulesets-fonts input")) {
+		console.debug("[Bluelib] Enabling interaction on:", element)
+		element.disabled = false
+		element.addEventListener("input", onFontRulesetSelection)
+		element.parentElement.classList.remove("fade")
+	}
+	for(const element of document.querySelectorAll("#panel-selectorsets input")) {
+		console.debug("[Bluelib] Enabling interaction on:", element)
+		element.disabled = false
+		element.addEventListener("input", onSelectorsetSelection)
+		element.parentElement.classList.remove("fade")
+	}
+	unpkgTargets.classList.remove("fade")
+	yarnTargets.classList.remove("fade")
+	console.info("[Bluelib] Example page is ready!")
+}
+
+
+/**
+ * Event callback for ruleset toggles in the page.
+ *
+ * @param event {Event} The triggering event.
+ */
+function onToggleableRulesetToggle(event) {
+	const baseName = event.target.value
+	const shouldBeEnabled = event.target.checked
+	console.debug("[Bluelib] Received onRulesetToggle ", baseName, shouldBeEnabled)
+	setRuleset(baseName, shouldBeEnabled)
+	sortRulesetsEnabled()
+	sync()
+}
+
+/**
+ * Event callback for color ruleset selections in the page.
+ * 
+ * @param event {Event} The triggering event.
+ */
+function onColorRulesetSelection(event) {
+	const old = rulesetsEnabled.find(v => v.startsWith("colors-"))
+	if(old) {
+		disableRuleset(old)
+	}
+	if(event.target.value) {
+		const baseName = `colors-${event.target.value}`
+		enableRuleset(baseName)
+	}
+	sync()
+}
+
+
+/**
+ * Event callback for font ruleset selections in the page.
+ * 
+ * @param event {Event} The triggering event.
+ */
+function onFontRulesetSelection(event) {
+	const old = rulesetsEnabled.find(v => v.startsWith("fonts-"))
+	if(old) {
+		disableRuleset(old)
+	}
+	if(event.target.value) {
+		const baseName = `fonts-${event.target.value}`
+		enableRuleset(baseName)
+	}
+	sync()
+}
+
+/**
+ * Event callback for selectorset selections in the page.
+ * 
+ * @param event {Event} The triggering event.
+ */
+function onSelectorsetSelection(event) {
+	selectorsetEnabled = event.target.value
+	console.debug("[Bluelib] Changed selectorset to:", selectorsetEnabled)
+	sync()
+}
+
+/**
+ * Event callback for {@link window} load.
+ */
+async function onWindowLoad() {
+	await initVersion()
 	await sleepUntilLessIsDone()
-
-	lessStyles = findLessStyles()
-	console.debug("Found Less stylesheets:", lessStyles)
-
-	lessColors = findLessColors()
-	console.debug("Found Less colors:", lessColors)
-
-	lessFonts = findLessFonts()
-	console.debug("Found Less fonts:", lessFonts)
-
-	background = findBackground()
-	console.debug("Found background:", background)
-
-	for(const [k, v] of Object.entries(lessStyles)) {
-		v.disabled = !enabledByDefault.includes(k)
-	}
-
-	for(const input of document.querySelectorAll(".ruleset-toggle")) {
-		input.disabled = false
-		input.parentElement.classList.remove("fade")
-	}
+	initStyleFromBaseName()
+	initTargets()
+	sortRulesetsEnabled()
+	sync()
+	allowInteraction()
 }
 
 
-function toggleStyle(name) {
-	if(lessStyles === undefined) {
-		console.error("Less stylesheets are not yet available.")
-		return
-	}
-
-	const style = lessStyles[name]
-	if(style === undefined) {
-		console.error("No such Less stylesheet.")
-		return
-	}
-
-	style.disabled = !style.disabled
-}
-
-
-function selectColor(name, bgsrc) {
-	if(lessColors === undefined) {
-		console.error("Less stylesheets are not yet available.")
-		return
-	}
-
-	const style = lessStyles[name]
-	if(style === undefined) {
-		console.error("No such Less stylesheet.")
-		return
-	}
-
-	for(const c of Object.values(lessColors)) {
-		c.disabled = true
-	}
-
-	style.disabled = false
-	background.src = bgsrc
-}
-
-
-enableChanges()
+window.onload = onWindowLoad
